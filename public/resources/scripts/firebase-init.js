e98b4ed3c4e1e0febed31dd2dc992b648fae68cb
Siddhant <siddhantvinchurkar@gmail.com>
2019-12-19 16:32:47 +0530

    Stuff

--- public/resources/scripts/firebase-init.js
+++ public/resources/scripts/firebase-init.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-	apiKey: "AIzaSyAOMfEBPfTxZJfUJOLSwNZvRdnrNncyuH8",
-	authDomain: "hyperphase.firebaseapp.com",
-	databaseURL: "https://hyperphase.firebaseio.com",
-	projectId: "hyperphase",
-	storageBucket: "hyperphase.appspot.com",
-	messagingSenderId: "1069304614624",
-	appId: "1:1069304614624:web:475b75635de64133ed8a85",
-	measurementId: "G-PLQBDX0YCY"
+	apiKey: "AIzaSyD1Iz_74EZFY0YnYPzTUgP-5ikshIGxp_U",
+	authDomain: "aiwcbangalore.firebaseapp.com",
+	databaseURL: "https://aiwcbangalore.firebaseio.com",
+	projectId: "aiwcbangalore",
+	storageBucket: "aiwcbangalore.appspot.com",
+	messagingSenderId: "368426264616",
+	appId: "1:368426264616:web:a363e8f4888a1286f52d1d",
+	measurementId: "G-8T9HR2WRQP"
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
---
+++ public/resources/scripts/main.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+
+	/* A function to lazy-load
+	 javascript files */
+
+	function loadJS(file) {
+
+		// DOM: Create the script element
+
+		var jsElm = document.createElement("script");
+
+		// set the type attribute
+
+		jsElm.type = "application/javascript";
+
+		// make the script element load file
+
+		jsElm.src = file;
+
+		// finally insert the element to the body element in order to load the script
+
+		document.body.appendChild(jsElm);
+	}
+
+	/* Lazy-load necessary scripts */
+
+	loadJS('resources/scripts/jquery.min.js');
+	loadJS('resources/scripts/jquery-ui.min.js');
+	loadJS('resources/scripts/materialize.min.js');
+	loadJS('resources/scripts/sweetalert.js');
+	loadJS('resources/scripts/firebase-app.js');
+	setTimeout(function () {
+		loadJS('resources/scripts/firebase-analytics.js');
+		loadJS('resources/scripts/firebase-auth.js');
+		loadJS('resources/scripts/firebase-firestore.js');
+		setTimeout(function () {
+			loadJS('resources/scripts/firebase-init.js');
+		}, 3000);
+	}, 3000);
+}
