c4e7b25aa941e1c9476dd9ac662b10a50eb3b92a
Siddhant <siddhantvinchurkar@gmail.com>
2019-12-20 21:23:25 +0530

    Mock-up ready

--- public/resources/scripts/main.js
+++ public/resources/scripts/main.js
@@ -1,4 +1,18 @@
 var email = 'Failed To Collect!';
+var email_content = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html style="width:100%;font-family:arial, \'helvetica neue\', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0;"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>New email 6</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--><style type="text/css">' +
+	'@media only screen and (max-width:600px) {p, ul li, ol li, a { font-size:16px!important; line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120%!important } h2 { font-size:26px!important; text-align:center; line-height:120%!important } h3 { font-size:20px!important; text-align:center; line-height:120%!important } h1 a { font-size:30px!important } h2 a { font-size:26px!important } h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { ' +
+	'text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button { font-size:20px!important; display:block!important; border-width:10px 0px 10px 0px!important } .es-btn-fw { border-width:10px 0px!important; text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { ' +
+	'padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } .es-desk-hidden { display:table-row!important; width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } .es-desk-menu-hidden { display:table-cell!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }#outlook a {	padding:0;}.ExternalClass {	width:100%;}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div {	line-height:100%;}.es-button ' +
+	'{	mso-style-priority:100!important;	text-decoration:none!important;}a[x-apple-data-detectors] {	color:inherit!important;	text-decoration:none!important;	font-size:inherit!important;	font-family:inherit!important;	font-weight:inherit!important;	line-height:inherit!important;}.es-desk-hidden {	display:none;	float:left;	overflow:hidden;	width:0;	max-height:0;	line-height:0;	mso-hide:all;}</style></head><body style="width:100%;font-family:arial, \'helvetica neue\', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0;"><div class="es-wrapper-color" style="background-color:#F6F6F6;"> <!--[if gte mso 9]><v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t"> <v:fill type="tile" color="#f6f6f6"></v:fill> </v:background><![endif]-->' +
+	'<table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;"><tr style="border-collapse:collapse;"><td valign="top" style="padding:0;Margin:0;"><table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;"><tr style="border-collapse:collapse;"><td align="center" style="padding:0;Margin:0;"><table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;"><tr style="border-collapse:collapse;">' +
+	'<td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;"> <!--[if mso]><table width="560" cellpadding="0" cellspacing="0"><tr><td width="180" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left;"><tr style="border-collapse:collapse;"><td width="180" class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;"><table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;"><td align="center" style="padding:0;Margin:0;">' +
+	'<img class="adapt-img" src="https://aiwcbangalore.org/resources/images/icons/favicon_512.png" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;" width="180" height="180"></td></tr></table></td></tr></table> <!--[if mso]></td><td width="20"></td><td width="360" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;"><td width="360" align="left" style="padding:0;Margin:0;"><table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;"><td align="left" style="padding:0;Margin:0;">' +
+	'<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:72px;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:108px;color:#333333;"><b>AIWC</b></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:72px;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:108px;color:#333333;"><b><span style="font-size:20px;">Bangalore</span></b><br></p></td></tr></table></td></tr></table> <!--[if mso]></td></tr></table><![endif]--></td></tr><tr style="border-collapse:collapse;"><td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;"><table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;">' +
+	'<td width="560" valign="top" align="center" style="padding:0;Margin:0;"><table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;"><td align="center" style="padding:20px;Margin:0;"><table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;"><td style="padding:0;Margin:0px 0px 0px 0px;border-bottom:1px solid #CCCCCC;background:none;height:1px;width:100%;margin:0px;"></td></tr></table></td></tr><tr style="border-collapse:collapse;"><td align="left" style="padding:0;Margin:0;">' +
+	'<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:36px;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:54px;color:#333333;"><strong>Welcome!</strong></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:21px;color:#333333;"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:helvetica, \'helvetica neue\', arial, verdana, sans-serif;line-height:21px;color:#333333;"><strong>You\'re now on the official AIWC Bangalore Mailing List.</strong></p>' +
+	'<p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:helvetica, \'helvetica neue\', arial, verdana, sans-serif;line-height:21px;color:#333333;"><strong>This entitles you to latest news and updates from AIWC.</strong></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:arial, \'helvetica neue\', helvetica, sans-serif;line-height:21px;color:#333333;"><br></p></td></tr><tr style="border-collapse:collapse;"><td align="center" style="padding:20px;Margin:0;"><table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;"><tr style="border-collapse:collapse;">' +
+	'<td style="padding:0;Margin:0px 0px 0px 0px;border-bottom:1px solid #CCCCCC;background:none;height:1px;width:100%;margin:0px;"></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table></div></body>' +
+	'</html>';
 
 window.onload = function () {
 
@@ -47,7 +61,32 @@ window.onload = function () {
 		loadJS('resources/scripts/firebase-performance.js');
 		loadJS('resources/scripts/firebase-messaging.js');
 		setTimeout(function () {
-			loadJS('resources/scripts/firebase-init.js');
+			/* Initialise Firebase */
+
+			var firebaseConfig = {
+				apiKey: "AIzaSyD1Iz_74EZFY0YnYPzTUgP-5ikshIGxp_U",
+				authDomain: "aiwcbangalore.firebaseapp.com",
+				databaseURL: "https://aiwcbangalore.firebaseio.com",
+				projectId: "aiwcbangalore",
+				storageBucket: "aiwcbangalore.appspot.com",
+				messagingSenderId: "368426264616",
+				appId: "1:368426264616:web:a363e8f4888a1286f52d1d",
+				measurementId: "G-8T9HR2WRQP"
+			};
+			firebase.initializeApp(firebaseConfig);
+			firebase.analytics();
+			firebase.auth().useDeviceLanguage();
+			var perf = firebase.performance();
+			var messaging = firebase.messaging();
+			messaging.usePublicVapidKey("BN9nMIAer_ewk7AwkSPlpMGIE5SBjnV2x5gwhGqXTR2sfd9lvlSuhICcXklPsj76ULTV9qP_HftuvVjxvl4J0SE");
+			Notification.requestPermission().then((permission) => {
+				if (permission === 'granted') {
+					// TODO: Retrieve an Instance ID token for use with FCM.
+				} else {
+					console.log('Unable to get permission to notify.');
+				}
+			});
+			var db = firebase.firestore();
 
 			/* Kill UI Blocker */
 
@@ -107,9 +146,54 @@ window.onload = function () {
 					/* Handle what happens when someone clicks the "Submit" button */
 
 					$('#submit').click(function () {
-						var db = firebase.firestore();
-						db.collection('email_list').add({ email: email }).then(function (docId) {
-							swal('Hello');
+						M.Modal.getInstance(progressDialogModal).open();
+						var existBool = false;
+						var docId = 'unknown_variable';
+						db.collection('email_list').where('email', '==', email).get().then(function (querySnapshot) {
+							querySnapshot.forEach(function (doc) {
+								existBool = true;
+								docId = doc.id;
+							});
+							if (existBool) {
+								db.collection('email_list').doc(docId).update({ email: email, updated_on: new Date(), }).then(function () {
+									db.collection('mail').add({
+										from: 'AIWC Bangalore <noreply@aiwcbangalore.org>',
+										to: email,
+										message: {
+											subject: 'Welcome to AIWC!',
+											text: '',
+											html: email_content
+										}
+									}).then(function () {
+										Swal.fire('Perfect!', 'We\'ll keep you updated.', 'success');
+										M.Modal.getInstance(progressDialogModal).close();
+									}).catch(function (error) {
+										console.error(error);
+									});
+								}).catch(function (error) {
+									console.error(error);
+								});
+							}
+							else {
+								db.collection('email_list').add({ email: email, created_on: new Date() }).then(function () {
+									db.collection('mail').add({
+										from: 'AIWC Bangalore <noreply@aiwcbangalore.org>',
+										to: email,
+										message: {
+											subject: 'Welcome to AIWC!',
+											text: '',
+											html: email_content
+										}
+									}).then(function () {
+										Swal.fire('Perfect!', 'We\'ll keep you updated.', 'success');
+										M.Modal.getInstance(progressDialogModal).close();
+									}).catch(function (error) {
+										console.error(error);
+									});
+								}).catch(function (error) {
+									console.error(error);
+								});
+							}
 						}).catch(function (error) {
 							console.error(error);
 						});
